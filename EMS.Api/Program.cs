024c2141041845d59ccea877198decf3a09f6463
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-25 04:32:29 +0100

    Updated Program.cs with AddApplication, AddInfrastructure dependencies and filter configuration

--- EMS.Api/Program.cs
+++ EMS.Api/Program.cs
@@ -1,10 +1,16 @@
+using EMS.Api.Filters;
+using EMS.Application;
+using EMS.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Filters.Add<ApiExceptionFilterAttribute>());
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
