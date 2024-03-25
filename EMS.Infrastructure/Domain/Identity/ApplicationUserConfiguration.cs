71ac46d9419dd79ed558acf7bedfad88126e9ef5
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-25 04:20:28 +0100

    Created ApplicationUserConfiguration

---
+++ EMS.Infrastructure/Domain/Identity/ApplicationUserConfiguration.cs
@@ -0,0 +1,55 @@
+using EMS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EMS.Infrastructure.Domain.Identity;
+
+public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    private const string AdminId = "4DAF65CB-CC0E-4C81-9183-20097EA81F5A";
+    
+    
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.ToTable("Users");
+        
+        
+        var admin = new ApplicationUser
+        {
+
+            Id = AdminId,
+            UserName = "nikola@email.com",
+            NormalizedUserName = "NIKOLA@EMAIL.COM",
+            Email = "nikola@email.com",
+            NormalizedEmail = "NIKOLA@EMAIL.COM",
+            EmailConfirmed = true,
+            PhoneNumberConfirmed = true,
+            SecurityStamp = new Guid().ToString("D"),
+            FirstName = "Nikola",
+            LastName = "Lelekovic",
+            ConcurrencyStamp = "c188a435-cfc8-45fd-836c-9a18bb9de405",
+            AccessFailedCount = 0
+        };
+
+        builder.HasData(admin);
+
+        builder
+            .HasMany(x => x.Roles)
+            .WithOne()
+            .HasForeignKey(x => x.UserId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        builder.HasMany(u => u.Events)
+            .WithOne(eau => eau.User)
+            .HasForeignKey(eau => eau.UserId)
+            .OnDelete(DeleteBehavior.Cascade); 
+
+        builder.HasMany(u => u.Reservations)
+            .WithOne(r => r.User)
+            .HasForeignKey(r => r.UserId)
+            .OnDelete(DeleteBehavior.Cascade); 
+
+
+    }
+}
