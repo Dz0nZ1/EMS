d9210720eab7bf1873892e91a8f7e704733c0fe6
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-25 04:30:54 +0100

    Created LocationController

---
+++ EMS.Api/Controller/LocationController.cs
@@ -0,0 +1,19 @@
+using EMS.Application.Common.Dto.Location;
+using EMS.Application.Location.Commands.CreateLocationCommand;
+using EMS.Application.Location.Queries.GetLocationDetailsQuery;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.Api.Controller;
+
+public class LocationController : ApiControllerBase
+{
+    
+    [HttpGet]
+    public async Task<ActionResult<LocationDetailsDto>> GetLocationDetails([FromQuery] GetLocationDetailsQuery query) =>
+        Ok(await Mediator.Send(query));
+    
+    [HttpPost]
+    public async Task<ActionResult<LocationDetailsDto>> CreateLocation(CreateLocationCommand command) =>
+        Ok(await Mediator.Send(command));
+    
+}
