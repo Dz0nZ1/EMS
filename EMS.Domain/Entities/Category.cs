9282880e1672f9e4f91120e4b84ecfd976a8143c
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-25 04:22:25 +0100

    Created Category Entity

---
+++ EMS.Domain/Entities/Category.cs
@@ -0,0 +1,27 @@
+namespace EMS.Domain.Entities;
+
+public class Category
+{
+   #region Properties
+
+   public Guid Id { get; private set; }
+   public string Name { get; private set; } = string.Empty;
+   public string Description { get; private set; } = string.Empty;
+
+   public IList<Event>? Events = new List<Event>();
+
+   #endregion
+
+   #region Constructors
+
+   public Category(string name, string description)
+   {
+      Id = Guid.NewGuid();
+      Name = name;
+      Description = description;
+   }
+   
+   private Category(){}
+
+   #endregion
+}
