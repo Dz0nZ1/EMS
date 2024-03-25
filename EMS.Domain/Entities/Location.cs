1a387f7a43991f3686265733b9540643f54cfd1e
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-25 04:30:17 +0100

    Created Location Entity

---
+++ EMS.Domain/Entities/Location.cs
@@ -0,0 +1,32 @@
+namespace EMS.Domain.Entities;
+
+public class Location
+{
+    #region Properties
+
+    public Guid Id { get; private set; }
+    public string Name { get; private set; } = string.Empty;
+    public string Country { get; private set; } = string.Empty;
+    public string City { get; private set; } = string.Empty;
+    public string Address { get; private set; } = string.Empty;
+    
+    public IList<Event>? Events = new List<Event>();
+    
+    #endregion
+
+
+    #region Constructors
+
+    public Location(string name, string country, string city, string address)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Country = country;
+        City = city;
+        Address = address;
+    }
+    
+    private Location(){}
+
+    #endregion
+}
