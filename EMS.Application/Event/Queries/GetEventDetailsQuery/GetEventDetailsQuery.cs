bb31c1ed310c03322b72932144616d7d1291bc2e
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-26 09:50:41 +0100

    Created GetEventDetailsQuery.cs

---
+++ EMS.Application/Event/Queries/GetEventDetailsQuery/GetEventDetailsQuery.cs
@@ -0,0 +1,6 @@
+﻿using EMS.Application.Common.Dto.Event;
+using MediatR;
+
+namespace EMS.Application.Event.Queries.GetEventDetailsQuery;
+
+public record GetEventDetailsQuery(string EventId) : IRequest<EventDetailsDto?>;
---
+++ EMS.Application/Event/Queries/GetEventDetailsQuery/GetEventDetailsQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using EMS.Application.Common.Dto.Event;
+using EMS.Application.Common.Exceptions;
+using EMS.Application.Common.interfaces;
+using EMS.Application.Common.Mappers.Event;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS.Application.Event.Queries.GetEventDetailsQuery;
+
+public class GetEventDetailsQueryHandler(IEmsDbContext dbContext) : IRequestHandler<GetEventDetailsQuery, EventDetailsDto?>
+{
+    public async Task<EventDetailsDto?> Handle(GetEventDetailsQuery request, CancellationToken cancellationToken)
+    {
+        var eventEntity = await dbContext.Events.Where(x => x.Id.ToString().Equals(request.EventId)).Include(x => x.Category).Include(x => x.Location)
+            .FirstOrDefaultAsync(cancellationToken: cancellationToken) ?? throw new NotFoundException("Event not found");
+
+        return eventEntity.ToDetailsDto();
+    }
+}
