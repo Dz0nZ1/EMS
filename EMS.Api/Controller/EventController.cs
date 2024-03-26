091b95f43233423911f55f0d025f58944660983c
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-26 09:45:44 +0100

    Created EventController

---
+++ EMS.Api/Controller/EventController.cs
@@ -0,0 +1,18 @@
+﻿using EMS.Application.Common.Dto.Event;
+using EMS.Application.Event.Commands.CreateEventCommand;
+using EMS.Application.Event.Queries.GetEventDetailsQuery;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.Api.Controller;
+
+public class EventController : ApiControllerBase
+{
+
+    [HttpGet]
+    public async Task<ActionResult<EventDetailsDto>> GetEventDetails([FromQuery] GetEventDetailsQuery query) =>
+        Ok(await Mediator.Send(query));
+
+    [HttpPost]
+    public async Task<ActionResult<EventDetailsDto>> CreateEvent(CreateEventCommand command) =>
+        Ok(await Mediator.Send(command));
+}
