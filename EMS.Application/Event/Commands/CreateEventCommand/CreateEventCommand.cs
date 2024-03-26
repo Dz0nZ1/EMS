c7afc1a64deb1844f257b28f853c38c6038e1fef
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-26 09:42:35 +0100

    Created CreateEventCommand

---
+++ EMS.Application/Event/Commands/CreateEventCommand/CreateEventCommand.cs
@@ -0,0 +1,6 @@
+﻿using EMS.Application.Common.Dto.Event;
+using MediatR;
+
+namespace EMS.Application.Event.Commands.CreateEventCommand;
+
+public record CreateEventCommand(CreateEventDto Event) : IRequest<EventDetailsDto?>;
