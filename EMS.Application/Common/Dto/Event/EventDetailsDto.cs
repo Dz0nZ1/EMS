7f2f838d964af30d6bad86b07b435d5a0417a805
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-26 09:46:18 +0100

    Created EventDetailsDto

---
+++ EMS.Application/Common/Dto/Event/EventDetailsDto.cs
@@ -0,0 +1,3 @@
+﻿namespace EMS.Application.Common.Dto.Event;
+
+public record EventDetailsDto(string Name, string Description, DateTime StartTime, DateTime EndTime, bool IsFree, decimal Price, string CategoryName, string CategoryDescription, string LocationName, string LocationCountry, string LocationCity, string LocationAddress);
