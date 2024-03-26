ca3913481cd99f707f76bdf1a326efd1afede7fa
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-26 13:16:49 +0100

    ReservationWebhook authorization

--- EMS.Api/Webhooks/ReservationWebhook.cs
+++ EMS.Api/Webhooks/ReservationWebhook.cs
@@ -1,8 +1,11 @@
+using EMS.Api.Auth.Constants;
 using EMS.Application.Reservation.Commands;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EMS.Api.Webhooks;
 
+[Authorize(AuthenticationSchemes = nameof(AuthConstants.HeaderBasicAuthenticationScheme))]
 public class ReservationWebhook : BaseWebhook
 {
     [HttpPost]
