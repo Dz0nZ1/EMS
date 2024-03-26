64fe4bc3ff0593cd825a0ae0653645f554f79afa
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-26 09:51:21 +0100

    Updated IEMSDbContext.cs

--- EMS.Application/Common/interfaces/IEMSDbContext.cs
+++ EMS.Application/Common/interfaces/IEMSDbContext.cs
@@ -5,7 +5,7 @@ namespace EMS.Application.Common.interfaces;
 
 public interface IEmsDbContext
 {
-    public DbSet<Event> Events { get; }
+    public DbSet<Domain.Entities.Event> Events { get; }
     public DbSet<Domain.Entities.Location> Locations { get; }
     public DbSet<Domain.Entities.Category> Categories { get; }
     public DbSet<Reservation> Reservations { get;}
