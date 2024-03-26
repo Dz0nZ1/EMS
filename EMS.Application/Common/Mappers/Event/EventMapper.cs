9458ad0d6b34fc698cd15e3379ca26b091fc0c4e
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-26 09:50:10 +0100

    Created EventMapper

---
+++ EMS.Application/Common/Mappers/Event/EventMapper.cs
@@ -0,0 +1,12 @@
+﻿using EMS.Application.Common.Dto.Event;
+using Riok.Mapperly.Abstractions;
+
+namespace EMS.Application.Common.Mappers.Event;
+
+[Mapper]
+public static partial class EventMapper
+{
+    public static partial Domain.Entities.Event ToEntity(this CreateEventDto dto);
+
+    public static partial EventDetailsDto ToDetailsDto(this Domain.Entities.Event entity);
+}
