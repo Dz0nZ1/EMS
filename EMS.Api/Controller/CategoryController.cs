fe73b7880e4f6ca80e3e54dcbe770e500a342112
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-27 23:42:50 +0100

    Created a new method for fetching CategoryList in CategoryController

--- EMS.Api/Controller/CategoryController.cs
+++ EMS.Api/Controller/CategoryController.cs
@@ -1,5 +1,6 @@
 using EMS.Application.Category.Commands.CreateCategoryCommand;
 using EMS.Application.Category.Commands.DeleteCategoryCommand;
+using EMS.Application.Category.Queries.GetCategoryDetailsListQuery;
 using EMS.Application.Category.Queries.GetCategoryDetailsQuery;
 using EMS.Application.Common.Dto.Category;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +13,10 @@ public class CategoryController : ApiControllerBase
     [HttpGet]
     public async Task<ActionResult<CategoryDetailsDto>> GetCategoryDetails([FromQuery] GetCategoryDetailsQuery query) =>
         Ok(await Mediator.Send(query));
+
+    [HttpGet]
+    public async Task<ActionResult<List<CategoryDetailsDto>>> GetCategoryDetailsList(
+        [FromQuery] GetCategoryDetailsListQuery query) => Ok(await Mediator.Send(query));
     
     [HttpPost]
     public async Task<ActionResult<CategoryDetailsDto>> CreateCategory(CreateCategoryCommand command) =>
