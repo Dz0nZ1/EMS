ba0b79c02ce57e9d444fd6671a1252e792cc3f3f
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-28 18:08:18 +0100

    Updated GetEventDetailsQueryHandler with reservations and users

--- EMS.Application/Event/Queries/GetEventDetailsQuery/GetEventDetailsQueryHandler.cs
+++ EMS.Application/Event/Queries/GetEventDetailsQuery/GetEventDetailsQueryHandler.cs
@@ -11,7 +11,11 @@ public class GetEventDetailsQueryHandler(IEmsDbContext dbContext) : IRequestHand
 {
     public async Task<EventDetailsDto?> Handle(GetEventDetailsQuery request, CancellationToken cancellationToken)
     {
-        var eventEntity = await dbContext.Events.Where(x => x.Id.ToString().Equals(request.EventId)).Include(x => x.Category).Include(x => x.Location)
+        var eventEntity = await dbContext.Events.Where(x => x.Id.ToString().Equals(request.EventId))
+            .Include(x => x.Category)
+            .Include(x => x.Location)
+            .Include(x => x.Reservations)
+            .Include(x => x.Users)
             .FirstOrDefaultAsync(cancellationToken: cancellationToken) ?? throw new NotFoundException("Event not found");
 
         return eventEntity.ToDetailsDto();
