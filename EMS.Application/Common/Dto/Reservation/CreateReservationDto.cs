73f32b03b869f73ab8dc5d618dc26711030386c5
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-29 00:42:02 +0100

    Updated CreateReservationDto with a List of EventIds

--- EMS.Application/Common/Dto/Reservation/CreateReservationDto.cs
+++ EMS.Application/Common/Dto/Reservation/CreateReservationDto.cs
@@ -1,3 +1,3 @@
 namespace EMS.Application.Common.Dto.Reservation;
 
-public record CreateReservationDto(DateTime ReservationDate, decimal Price, bool hasCupon, Guid EventId, string UserId);
+public record CreateReservationDto(DateTime ReservationDate, decimal Price, bool HasCoupon, List<Guid> EventIds, string UserId);
