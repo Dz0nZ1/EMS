ea1b21cd95e3a600eb8190ba86b2bcb17d4e3afb
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-29 00:43:40 +0100

    Updated EventDetailsDto with EventInfoDto

--- EMS.Application/Common/Dto/Event/EventDetailsDto.cs
+++ EMS.Application/Common/Dto/Event/EventDetailsDto.cs
@@ -1,3 +1,25 @@
-﻿namespace EMS.Application.Common.Dto.Event;
+﻿using EMS.Application.Common.Dto.Category;
+using EMS.Application.Common.Dto.Location;
+using EMS.Application.Common.Dto.Reservation;
+using EMS.Application.Common.Dto.Users;
+using EMS.Domain.Entities;
 
-public record EventDetailsDto(string Name, string Description, DateTime StartTime, DateTime EndTime, bool IsFree, decimal Price, string CategoryName, string CategoryDescription, string LocationName, string LocationCountry, string LocationCity, string LocationAddress);
+namespace EMS.Application.Common.Dto.Event;
+
+public record EventDetailsDto(
+    string Name,
+    string Description,
+    DateTime StartTime,
+    DateTime EndTime,
+    bool IsFree,
+    decimal Price, EventInfoDto EventInfoDto)
+
+{
+    
+    internal EventDetailsDto AddEventInfo(EventInfoDto eventInfoDto)
+    {
+        return this with { EventInfoDto = eventInfoDto };
+    }
+    
+    
+}
