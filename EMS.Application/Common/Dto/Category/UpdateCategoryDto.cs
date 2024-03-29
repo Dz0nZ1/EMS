909c604e82d0f85f5d8b973937298cdb3ce9f07a
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-30 00:36:07 +0100

    Added CategoryId to UpdateCategoryDto

--- EMS.Application/Common/Dto/Category/UpdateCategoryDto.cs
+++ EMS.Application/Common/Dto/Category/UpdateCategoryDto.cs
@@ -1,3 +1,3 @@
 namespace EMS.Application.Common.Dto.Category;
 
-public record UpdateCategoryDto (string? Name, string? Description);
+public record UpdateCategoryDto (string CategoryId, string Name, string Description);
