7bc0f072e57b59ccca113b2399a995e3c62c2379
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-30 00:34:30 +0100

    Created GetReservationDetails, UpdateReservation and DeleteReservation methods in ReservationController

--- EMS.Api/Controller/ReservationController.cs
+++ EMS.Api/Controller/ReservationController.cs
@@ -1,17 +1,37 @@
 using EMS.Application.Common.Dto.Reservation;
 using EMS.Application.Reservation.Commands;
+using EMS.Application.Reservation.Commands.CreateReservationCommand;
+using EMS.Application.Reservation.Commands.DeleteReservationCommand;
+using EMS.Application.Reservation.Commands.UpdateReservationCommand;
 using EMS.Application.Reservation.Queries;
+using EMS.Application.Reservation.Queries.GetReservationDetailsListQuery;
+using EMS.Application.Reservation.Queries.GetReservationDetailsQuery;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EMS.Api.Controller;
 
 public class ReservationController : ApiControllerBase
 {
+    
+    [HttpGet]
+    public async Task<ActionResult<List<ReservationDetailsDto>>> GetReservationDetailsList([FromQuery] GetReservationDetailsListQuery query) =>
+        Ok(await Mediator.Send(query));
+
+    [HttpGet]
+    public async Task<ActionResult<ReservationDetailsDto?>> GetReservationDetails(
+        [FromQuery] GetReservationDetailsQuery query) => Ok(await Mediator.Send(query));
+    
     [HttpPost]
     public async Task<ActionResult<ReservationDetailsDto?>> CreateReservation(CreateReservationCommand command) =>
         Ok(await Mediator.Send(command));
 
-    [HttpGet]
-    public async Task<ActionResult<ReservationDetailsDto?>> GetReservationDetailsList([FromQuery] GetReservationDetailsListQuery query) =>
-        Ok(await Mediator.Send(query));
+    [HttpPut]
+    public async Task<ActionResult<ReservationDetailsDto?>> UpdateReservation(UpdateReservationCommand command) =>
+        Ok(await Mediator.Send(command));
+
+
+    [HttpDelete]
+    public async Task<ActionResult<string?>> DeleteReservation(DeleteReservationCommand command) =>
+        Ok(await Mediator.Send(command));
+
 }
