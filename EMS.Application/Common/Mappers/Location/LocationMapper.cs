8c8ae6042153b9adfed70584d9dcb7c725110fab
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-30 00:32:49 +0100

    Updated LocationMapper with ToEntity method for UpdateLocationDto

--- EMS.Application/Common/Mappers/Location/LocationMapper.cs
+++ EMS.Application/Common/Mappers/Location/LocationMapper.cs
@@ -10,5 +10,12 @@ public static partial class LocationMapper
     
     public static partial LocationDetailsDto ToDetailsDto(this Domain.Entities.Location entity);
 
-    public static partial List<LocationDetailsDto> ToDetailsList(this List<Domain.Entities.Location> locations);
+    public static partial List<LocationDetailsDto> ToDetailsList(this List<Domain.Entities.Location> entities);
+
+    public static void ToEntity(this Domain.Entities.Location entity, UpdateLocationDto dto)
+    {
+        entity.UpdateLocation(dto.Name, dto.Country, dto.City, dto.Address);
+    }
+
+ 
 }
