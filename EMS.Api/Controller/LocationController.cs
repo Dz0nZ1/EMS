7dd699b3e95f978330086aa2cfadf4a898ea6f29
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-29 01:28:34 +0100

    Created GetLocationDetailsList method in LocationController

--- EMS.Api/Controller/LocationController.cs
+++ EMS.Api/Controller/LocationController.cs
@@ -1,5 +1,6 @@
 using EMS.Application.Common.Dto.Location;
 using EMS.Application.Location.Commands.CreateLocationCommand;
+using EMS.Application.Location.Queries.GetLocationDetailsListQuery;
 using EMS.Application.Location.Queries.GetLocationDetailsQuery;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +12,12 @@ public class LocationController : ApiControllerBase
     [HttpGet]
     public async Task<ActionResult<LocationDetailsDto>> GetLocationDetails([FromQuery] GetLocationDetailsQuery query) =>
         Ok(await Mediator.Send(query));
+
+
+    [HttpGet]
+    public async Task<ActionResult<List<LocationDetailsDto>>> GetLocationDetailsList(
+        [FromQuery] GetLocationDetailsListQuery query) => Ok(await Mediator.Send(query));
+    
     
     [HttpPost]
     public async Task<ActionResult<LocationDetailsDto>> CreateLocation(CreateLocationCommand command) =>
