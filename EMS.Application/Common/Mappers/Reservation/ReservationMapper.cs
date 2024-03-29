fc41d0bdb87d0f126579089366f1f8621dc331c7
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-29 01:21:51 +0100

    Created ToReservationDetails method in ReservationMapper

--- EMS.Application/Common/Mappers/Reservation/ReservationMapper.cs
+++ EMS.Application/Common/Mappers/Reservation/ReservationMapper.cs
@@ -1,4 +1,5 @@
 using EMS.Application.Common.Dto.Reservation;
+using EMS.Application.Common.Dto.Users;
 using Riok.Mapperly.Abstractions;
 
 namespace EMS.Application.Common.Mappers.Reservation;
@@ -7,8 +8,23 @@ namespace EMS.Application.Common.Mappers.Reservation;
 public static partial class ReservationMapper
 {
     public static partial Domain.Entities.Reservation ToEntity(this CreateReservationDto dto);
-    
-    public static partial ReservationDetailsDto ToDetailsDto(this Domain.Entities.Reservation entity);
+
+    public static ReservationDetailsDto ToReservationDetailsDto(this Domain.Entities.Reservation entity)
+    {
+        return new ReservationDetailsDto(entity.ReservationDate, entity.Price, entity.hasCoupon, new UserDetailsDto(entity.User.FirstName, entity.User.LastName));
+    }
+
+    public static List<ReservationDetailsDto> ToReservationDetailsList(
+        this List<Domain.Entities.Reservation> reservations)
+    {
+        var newDtoList = new List<ReservationDetailsDto>();
+        foreach (var reservation in reservations)
+        {
+            newDtoList.Add(reservation.ToReservationDetailsDto());
+        }
+
+        return newDtoList;
+    }
 
     public static partial List<ReservationDetailsDto> ToListDto(this List<Domain.Entities.Reservation> reservations);
 }
