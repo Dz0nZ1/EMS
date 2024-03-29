bf4aead68a1883bdc0bdb4f7565a5544c12a2ea2
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-29 01:03:46 +0100

    Updated Migration

---
+++ EMS.Infrastructure/Migrations/20240328235214_ReservationColumnNameFix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReservationColumnNameFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                    name: "hasCoupon",
+                    table: "Reservations",
+                    type: "boolean",
+                    nullable: false
+                );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "hasCupon",
+                table: "Reservations");
+        }
+    }
+}
