e74538c9dd05fe65c66cd9d39a8808dbdb9457e9
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-30 00:36:39 +0100

    Added LocationId to UpdateLocationDto

--- EMS.Application/Common/Dto/Location/UpdateLocationDto.cs
+++ EMS.Application/Common/Dto/Location/UpdateLocationDto.cs
@@ -1,3 +1,3 @@
 namespace EMS.Application.Common.Dto.Location;
 
-public record UpdateLocationDto(string? Name, string? Country, string? City, string? Address);
+public record UpdateLocationDto(string LocationId, string Name, string Country, string City, string Address);
