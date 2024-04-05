8ab92098376b633c632a5ab12fc7735ee30e22d3
Dz0nZ1 <dzonzi777@gmail.com>
2024-04-05 20:16:28 +0200

    Created GetDetailsValidator

---
+++ EMS.Application/Common/Validators/GetDetailsValidator.cs
@@ -0,0 +1,13 @@
+using FluentValidation;
+
+namespace EMS.Application.Common.Validators;
+
+public class GetDetailsValidator : AbstractValidator<string>
+{
+    public GetDetailsValidator()
+    {
+        RuleFor(x => x)
+            .Matches(@"^({){0,1}[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}(}){0,1}$")
+            .WithMessage("ID is required.");
+    }
+}
