619a73ac49d78f415dc1e69543d82149d7e4df5d
Dz0nZ1 <dzonzi777@gmail.com>
2024-04-10 22:51:07 +0200

    Created GetReservationDetailsQueryValidator

---
+++ EMS.Application/Reservation/Queries/GetReservationDetailsQuery/GetReservationDetailsQueryValidator.cs
@@ -0,0 +1,12 @@
+using EMS.Application.Common.Validators;
+using FluentValidation;
+
+namespace EMS.Application.Reservation.Queries.GetReservationDetailsQuery;
+
+public class GetReservationDetailsQueryValidator : AbstractValidator<GetReservationDetailsQuery>
+{
+    public GetReservationDetailsQueryValidator()
+    {
+        RuleFor(x => x.ReservationId).SetValidator(new GetDetailsValidator());
+    }
+}
