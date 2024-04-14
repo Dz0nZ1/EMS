4aca78e6f74cf5e6375bea8bf39e8322c388eb3b
Dz0nZ1 <dzonzi777@gmail.com>
2024-04-14 15:36:26 +0200

    Created ReservationService implementation

---
+++ EMS.Infrastructure/Services/ReservationService.cs
@@ -0,0 +1,36 @@
+using EMS.Application.Common.Dto.Reservation;
+using EMS.Application.Common.Exceptions;
+using EMS.Application.Common.interfaces;
+using EMS.Application.Common.Mappers.Reservation;
+using EMS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS.Infrastructure.Services;
+
+public class ReservationService(IEmsDbContext dbContext) : IReservationService
+{
+    public async Task<ReservationDetailsDto?> CreateAsync(CreateReservationDto reservationDto, CancellationToken cancellationToken)
+    {
+        var user = await dbContext.Users.FirstOrDefaultAsync(x => x.Id == reservationDto.UserId, cancellationToken)
+                   ?? throw new NotFoundException("User not found");
+
+        var reservation = reservationDto.ToEntity().AddUser(user);
+        
+        await dbContext.Reservations.AddAsync(reservation, cancellationToken);
+        
+        foreach (var eventId in reservationDto.EventIds)
+        {
+            var eventEntity = await dbContext.Events.FirstOrDefaultAsync(x => x.Id == eventId, cancellationToken)
+                              ?? throw new NotFoundException($"Event with ID {eventId} not found");
+
+            var reservationEvent = new ReservationEvent();
+            reservationEvent.AddReservationEvent(eventEntity, reservation);
+
+            await dbContext.ReservationEvents.AddAsync(reservationEvent, cancellationToken);
+        }
+        
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return reservation.ToReservationDetailsDto();
+    }
+}
