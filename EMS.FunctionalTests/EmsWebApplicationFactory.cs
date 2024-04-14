38d44c2109939f246fe6778ff50892ec27cff38d
Dz0nZ1 <dzonzi777@gmail.com>
2024-04-14 15:33:57 +0200

    Added mock services to EmsWebApplicationFactory

--- EMS.FunctionalTests/EmsWebApplicationFactory.cs
+++ EMS.FunctionalTests/EmsWebApplicationFactory.cs
@@ -1,14 +1,19 @@
+using EMS.Application.Common.interfaces;
 using EMS.Infrastructure.Contexts;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Moq;
 
 namespace EMS.FunctionalTests;
 
 public class EmsWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
 {
+    // Creating new Mock Services
+    // public Mock<ICategoryService> MockCategoryService { get; } = new();
+    // public Mock<ILocationService> MockLocationService { get; } = new();
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
@@ -23,6 +28,10 @@ public class EmsWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup
                 {
                     options.UseInMemoryDatabase(dbName);
                 });
+                
+                //Overriding the real services with mock 
+                // services.AddScoped(_ => MockCategoryService.Object);
+                // services.AddScoped(_ => MockLocationService.Object);
             });
     }
 }
