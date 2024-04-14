56564d46b2510eff866c4afafbc4e33e69f01fc5
Dz0nZ1 <dzonzi777@gmail.com>
2024-04-14 15:37:39 +0200

    Added category service to CreateCategoryCommandHandler

--- EMS.Application/Category/Commands/CreateCategoryCommand/CreateCategoryCommandHandler.cs
+++ EMS.Application/Category/Commands/CreateCategoryCommand/CreateCategoryCommandHandler.cs
@@ -1,18 +1,13 @@
 using EMS.Application.Common.Dto.Category;
 using EMS.Application.Common.interfaces;
-using EMS.Application.Common.Mappers.Category;
 using MediatR;
 
 namespace EMS.Application.Category.Commands.CreateCategoryCommand;
 
-public class CreateCategoryCommandHandler(IEmsDbContext dbContext) : IRequestHandler<CreateCategoryCommand, CategoryDetailsDto?>
+public class CreateCategoryCommandHandler(ICategoryService categoryService) : IRequestHandler<CreateCategoryCommand, CategoryDetailsDto?>
 {
     public async Task<CategoryDetailsDto?> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
     {
-        var category = request.Category.ToEntity();
-        await dbContext.Categories.AddAsync(category, cancellationToken);
-        await dbContext.SaveChangesAsync(cancellationToken);
-        return category.ToDetailsDto();
-
+        return await categoryService.CreateAsync(request.Category, cancellationToken: cancellationToken);
     }
 }
