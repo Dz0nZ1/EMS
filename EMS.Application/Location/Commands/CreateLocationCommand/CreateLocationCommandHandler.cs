4055409b632b0f4b940451e26d55c896418171af
Dz0nZ1 <dzonzi777@gmail.com>
2024-04-14 15:39:09 +0200

    Added LocationService to CreateLocationCommandHandler

--- EMS.Application/Location/Commands/CreateLocationCommand/CreateLocationCommandHandler.cs
+++ EMS.Application/Location/Commands/CreateLocationCommand/CreateLocationCommandHandler.cs
@@ -1,17 +1,13 @@
 using EMS.Application.Common.Dto.Location;
 using EMS.Application.Common.interfaces;
-using EMS.Application.Common.Mappers.Location;
 using MediatR;
 
 namespace EMS.Application.Location.Commands.CreateLocationCommand;
 
-public class CreateLocationCommandHandler(IEmsDbContext dbContext) : IRequestHandler<CreateLocationCommand, LocationDetailsDto?>
+public class CreateLocationCommandHandler(ILocationService locationService) : IRequestHandler<CreateLocationCommand, LocationDetailsDto?>
 {
     public async Task<LocationDetailsDto?> Handle(CreateLocationCommand request, CancellationToken cancellationToken)
     {
-        var location = request.Location.ToEntity();
-        await dbContext.Locations.AddAsync(location, cancellationToken);
-        await dbContext.SaveChangesAsync(cancellationToken);
-        return location.ToDetailsDto();
+        return await locationService.CreateAsync(request.Location, cancellationToken: cancellationToken);
     }
 }
