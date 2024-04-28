5c46832e29874d9a0f10df5db284e361ca736077
Dz0nZ1 <dzonzi777@gmail.com>
2024-04-28 16:16:39 +0200

    Created AuthController

---
+++ EMS.Api/Controller/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using EMS.Application.Auth.Commands.BeginLoginCommand;
+using EMS.Application.Auth.Commands.CompleteLoginCommand;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.Api.Controller;
+
+public class AuthController : ApiControllerBase
+{
+    [AllowAnonymous]
+    [HttpPost]
+    public async Task<ActionResult> BeginLogin(BeginLoginCommand command) => Ok(await Mediator.Send(command));
+
+    [AllowAnonymous]
+    [HttpGet("{validationToken}/CompleteLogin")]
+    public async Task<ActionResult> CompleteLogin([FromRoute] string validationToken) => Ok(await Mediator.Send(new CompleteLoginCommand(validationToken)));
+}
