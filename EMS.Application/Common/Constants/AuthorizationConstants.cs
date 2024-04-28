5931f3aa0343eceb81beac42f0a1f7a16cb427df
Dz0nZ1 <dzonzi777@gmail.com>
2024-04-28 16:16:21 +0200

    Created AuthorizationConstants

---
+++ EMS.Application/Common/Constants/AuthorizationConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace EMS.Application.Common.Constants;
+
+public class AuthorizationConstants
+{
+    public const string Administrator = "Administrator";
+    public const string User = "User";
+}
